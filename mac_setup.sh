1e0ed7d6600ceb560d376eb128a47b80b3d099c1
Johan Mena <johan.menac@gmail.com>
2022-01-23 16:52:28 -0500

    cleanup

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,33 +1,16 @@
 # First install Xcode from the App Store and run it once. Then...
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install caskroom/cask/brew-cask
-
-brew install git imagemagick mongodb sbt scala wget openssl leiningen node docker
+brew install git wget openssl node docker
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-brew cask install java
-brew cask install dropbox
-
-brew cask install caffeine
-brew cask install transmission
-brew cask install the-unarchiver
-brew cask install spotify
-brew cask install vlc
-
-brew cask install google-chrome
-brew cask install skype
-brew cask install firefox
-brew cask install hipchat
-
-brew cask install source-tree
-brew cask install iterm2
-brew cask install vagrant
-brew cask install mongochef
-brew cask install macvim
-brew cask install intellij-idea
+brew install java
+brew install caffeine
+brew install iterm2
+brew install macvim
+brew install intellij-idea
 
 #Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0.02
